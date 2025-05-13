e0c7fe6cc6e22cb0166e997cc72adb920a08fa96
Ignetos <krisnain.dev@gmail.com>
2025-05-14 00:17:00 +0700

    first try at cli

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func cleanInput(text string) []string {
-	return strings.Fields(text)
+	input := strings.ToLower(text)
+	return strings.Fields(input)
 }
 
 func main() {
-	fmt.Println("Hello, World!")
-	cleanInput("text")
+	scanner := bufio.NewScanner(os.Stdin)
+	var text string
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		text = scanner.Text()
+		words := cleanInput(text)
+		if len(words) == 0 {
+			continue
+		}
+		firstWord := words[0]
+		fmt.Printf("Your command was: %s\n", firstWord)
+	}
 }
